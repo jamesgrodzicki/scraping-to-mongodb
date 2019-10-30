b78b1ab8b7023534a02bf21ba4f1ec4ebce8edd4
James Grodzicki <jamespgrodzicki@gmail.com>
2019-10-29 20:15:20 -0500

    setting up mvc

--- server.js
+++ server.js
@@ -1,2 +1,23 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const db = require('./models');
+
+const PORT = 3001;
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
 
 app.listen(PORT, () => console.log('app: localhost://' + PORT));
