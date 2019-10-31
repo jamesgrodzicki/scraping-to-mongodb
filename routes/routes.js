6b4ce5dcfbf88b67dc744578db78c4c59814c039
James Grodzicki <jamespgrodzicki@gmail.com>
2019-10-30 23:16:11 -0500

    fixed routing with mlab

--- routes/routes.js
+++ routes/routes.js
@@ -2,25 +2,35 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const db = require('../models');
 module.exports = app => {
-    app.get('/', function (req, res) {
+    app.get('/articles', function (req, res) {
         axios.get('https://www.theguardian.com/us').then(response => {
             const $ = cheerio.load(response.data);
             console.log('scraped');
             const result = [];
             $('a.fc-item__link').each(function (i, element) {
+
                 const data = {}
                 data.title = $(this).children('span.fc-item__kicker').text();
-                data.summary = $(this).children('span.fc-item__headline').text();
-                data.link = $(this).attr('href');
-                result.push(data);
+                if (data.title) {
+                    for (let j = 0; j < result.length; j++) {
+                        if (result[j].title == data.title) {
+                            return result;
+                        }
+                    }
+                    data.summary = $(this).children('span.fc-item__headline').text();
+                    data.link = $(this).attr('href');
+                    result.push(data);
+                }
+                else {
+                    return result;
+                }
             });
             console.log(result);
             db.Article.create(result).then(function (dbArticle) {
-                console.log(dbArticle);
+                res.json(dbArticle);
             }).catch(function (err) {
-                console.log(err);
+                res.json(err);
             });
-            return res.render('index', { result: result });
         });
     });
 
@@ -33,13 +43,13 @@ module.exports = app => {
             });
     });
 
-    app.post('/articles/:id', function(req, res){
-        db.Comment.create(req.body).then(function(dbComment){
+    app.post('/articles/:id', function (req, res) {
+        db.Comment.create(req.body).then(function (dbComment) {
             return db.Article.findOneAndUpdate(
-                {_id:req.params.id},{comments:dbComment._id},{new:true});
-        }).then(function(dbArticle){
+                { _id: req.params.id }, { comments: dbComment._id }, { new: true });
+        }).then(function (dbArticle) {
             res.json(dbArticle);
-        }).catch(function(err){
+        }).catch(function (err) {
             res.json(err);
         });
     });
